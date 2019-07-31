3b115c04e69adc19acab49c28f61a05b76130582
Tim Eichinger <business.timeichinger@gmail.com>
2019-07-31 08:48:46 +0200

    updated log-reg-process (2)

--- app/src/main/java/com/androidproject/univents/logreg/RegisterActivity.java
+++ app/src/main/java/com/androidproject/univents/logreg/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.androidproject.univents.logreg;
 
+import android.content.res.ColorStateList;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.PagerAdapter;
@@ -8,7 +9,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.Html;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +25,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidproject.univents.R;
+import com.androidproject.univents.customviews.NoSwipeViewPager;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -30,10 +34,15 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     private static final int POS_DATA_ENTRY = 0;
     private static final int POS_DATA_PROTECT = 1;
+    private static final String REGEX_PASSWORD_NUMBERS = ".*[0-9].*";
+    private static final String REGEX_PASSWORD_SMALL_LETTER = ".*[a-zäöüß].*";
+    private static final String REGEX_PASSWORD_BIG_LETTER = ".*[A-ZÄÖÜ].*";
+    private static final String REGEX_EMAIL = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:" +
+            "[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
 
     private ActionBar actionBar;
 
-    private ViewPager layout_pager;
+    private NoSwipeViewPager layout_pager;
     private View layoutDataEntry, layoutDataProtection;
     private ArrayList<View> registerLayouts = new ArrayList<>();
 
@@ -46,11 +55,16 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText txtFirstName, txtLastname, txtOrgaName, txtEmail, txtEmailConfirm
             , txtPassword, txtPasswordConfirm;
 
+    private boolean isEmailConfirm, isPwNumbers, isPwLetters, isPwNumCount, isPwConfirm;
+
     private ImageView imgConfirmEmail, imgConfirmPassword, imgMinLetter
             , imgMinNumber, imgMinChars;
 
     boolean isOrga = false;
 
+    int[][] states = new int[][] {
+            new int[] { android.R.attr.state_enabled}  // pressed
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +86,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
+        actionBar.setTitle(String.format(getString(R.string.toolbar_reg_steps), 1));
     }
 
     /**
@@ -123,6 +138,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                         changeIcons(true, R.drawable.ic_check_24dp);
                         break;
                 }
+                actionBar.setTitle(String.format(getString(R.string.toolbar_reg_steps),(i + 1)));
             }
 
             @Override
@@ -192,6 +208,127 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         imgMinLetter = layoutDataEntry.findViewById(R.id.img_passw_big_small_letters);
         imgMinNumber = layoutDataEntry.findViewById(R.id.img_passw_number);
 
+        setEditTextWatcher();
+
+    }
+
+    /**
+     * Sets TextWatchers to necessary edit-texts
+     */
+    private void setEditTextWatcher() {
+        setEmailConfirmWatcher();
+        setPasswordWatcher();
+        setPasswordConfirmWatcher();
+    }
+
+    /**
+     * Text-Watcher for EditText password confirm
+     */
+    private void setPasswordConfirmWatcher() {
+        txtPasswordConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean conditionPwConfirm = s.toString().equals(txtPassword.getText().toString())
+                        && !s.toString().isEmpty();
+
+                isPwConfirm = checkRealTimeInputValid(conditionPwConfirm, imgConfirmPassword);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+
+    /**
+     * Text-Watcher for EditText password
+     */
+    private void setPasswordWatcher() {
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean conditionCountChars = s.toString().length() >= 8;
+                boolean conditionNumbers = s.toString().matches(REGEX_PASSWORD_NUMBERS);
+                boolean conditionLetters = s.toString().matches(REGEX_PASSWORD_SMALL_LETTER)
+                        && s.toString().matches(REGEX_PASSWORD_BIG_LETTER);
+
+                isPwNumCount = checkRealTimeInputValid(conditionCountChars, imgMinChars);
+                isPwLetters = checkRealTimeInputValid(conditionLetters, imgMinLetter);
+                isPwNumbers = checkRealTimeInputValid(conditionNumbers, imgMinNumber);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    /**
+     * TextWatcher for EditText Email-Confirm
+     */
+    private void setEmailConfirmWatcher() {
+        txtEmailConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                boolean conditionPwConfirm = s.toString().equals(txtEmail.getText()
+                        .toString()) && !s.toString().isEmpty();
+
+                isEmailConfirm = checkRealTimeInputValid(conditionPwConfirm, imgConfirmEmail);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    /**
+     * checks whether in realtime whether the input is valid or not
+     * @param condition includes the condition for the input
+     * @param icon includes the icon, that should be changed when the input vaidation
+     *             changes
+     * @return returns whether the input is valid or not
+     */
+    private boolean checkRealTimeInputValid(boolean condition, ImageView icon) {
+        if (condition) {
+            setIconTrue(icon);
+            return true;
+        } else {
+            setIconFalse(icon);
+            return false;
+        }
+    }
+
+    private void setIconTrue(ImageView icon) {
+        icon.setImageTintList(new ColorStateList(states
+                , new int[]{getResources().getColor(R.color.colorAccent)}));
+        icon.setImageResource(R.drawable.ic_check_24dp);
+    }
+
+    private void setIconFalse(ImageView icon) {
+        icon.setImageTintList(new ColorStateList(states
+                , new int[]{getResources().getColor(R.color.colorDarkText)}));
+        icon.setImageResource(R.drawable.ic_close_24dp);
     }
 
     @Override
@@ -217,36 +354,64 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     }
 
+    /**
+     * handles when the floating action button is clicked.
+     * It decides whether to go on with step two or finish
+     * sign in process.
+     */
     private void handleFabRegisterClick() {
 
         String firstName = txtFirstName.getText().toString();
         String lastName = txtLastname.getText().toString();
         String email = txtEmail.getText().toString();
-        String emailConfirm = txtEmailConfirm.getText().toString();
         String password = txtPassword.getText().toString();
-        String passwordConfirm = txtPasswordConfirm.getText().toString();
-        String orgaName = null;
+        String orgaName = "";
+
         if (isOrga) orgaName = txtOrgaName.getText().toString();
 
         if (layout_pager.getCurrentItem() == POS_DATA_ENTRY) {
-            if (checkCorrectFilled(firstName, lastName, orgaName, email, emailConfirm, password
-                    , passwordConfirm)) {
+            if (checkCorrectFilled(firstName, lastName, orgaName, email)) {
                 layout_pager.setCurrentItem(POS_DATA_PROTECT);
             }
         } else {
             if (checkDataProtection.isChecked()) {
                 signIn(firstName, lastName, orgaName, email, password);
             } else {
-                Toast.makeText(getApplicationContext(), getString(R.string.confirm_data_protection)
-                        , Toast.LENGTH_LONG).show();
+                showToast(getString(R.string.confirm_data_protection));
             }
         }
     }
 
-    //TODO check all edittext if they are valid
+    /**
+     * checks whether all inputs are valid when user wants to register
+     * @param firstName text from editText txtFirstName
+     * @param lastName text from editText txtLastName
+     * @param orgaName text from editText txtOrgaName
+     * @param email text from editText txtEmail
+     * @return returns if all inputs are valid
+     */
     private boolean checkCorrectFilled(String firstName, String lastName, String orgaName
-            , String email, String emailConfirm, String password, String passwordConfirm) {
-        return true;
+            , String email) {
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()
+                || (orgaName.isEmpty() && isOrga)) {
+            showToast(getString(R.string.fill_all_fields));
+            return false;
+        } else if (!email.matches(REGEX_EMAIL)) {
+            showToast(getString(R.string.no_valid_email));
+            return false;
+        } else if (!isEmailConfirm) {
+            showToast(getString(R.string.emails_are_not_same));
+            return false;
+        } else if (!(isPwLetters && isPwNumbers && isPwNumCount)) {
+            showToast(getString(R.string.password_not_valid));
+            return false;
+        } else if (isPwConfirm){
+            return true;
+        } else {
+            showToast(getString(R.string.passwords_not_same));
+            return false;
+        }
     }
 
     //TODO sign in via firebase and finish activity with result ok
@@ -271,7 +436,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
         return super.onOptionsItemSelected(item);
     }
-
     /**
      * Simple Custom-PagerAdapter that adds the views from an ArrayList
      * to the view-group and handles adapter-actions
@@ -280,7 +444,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         private ArrayList<View> views;
 
-        public ViewPagerAdapter(ArrayList<View> views) {
+        ViewPagerAdapter(ArrayList<View> views) {
             this.views = views;
         }
 
@@ -308,6 +472,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
             return view == o;
         }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
 }
