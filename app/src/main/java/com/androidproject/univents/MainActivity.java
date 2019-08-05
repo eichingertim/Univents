3de7f40ce06b4a5a5986c9ea0ac2185d329fd94d
Tim Eichinger <business.timeichinger@gmail.com>
2019-08-05 14:34:02 +0200

    updated strings.xml

--- app/src/main/java/com/androidproject/univents/MainActivity.java
+++ app/src/main/java/com/androidproject/univents/MainActivity.java
@@ -1,13 +1,37 @@
 package com.androidproject.univents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.androidproject.univents.logreg.LogRegChooserActivity;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        //Testbutton to SignOut
+        Button btnSignout = findViewById(R.id.btn_sign_out);
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logOut();
+                auth.signOut();
+                startActivity(new Intent(MainActivity.this, LogRegChooserActivity.class));
+                finish();
+            }
+        });
+
     }
 }
